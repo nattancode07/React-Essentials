4d561aa541284b295ee56864c9b5a3dd8026483f
Elyaquim Nattan <elyaquimnattan@gmail.com>
2024-10-24 13:39:10 -0300

    commit 5 organizing components

---
+++ src/components/Examples.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { EXAMPLES } from '../data-with-examples.js';
+import TabButton from '../components/TabButton';
+
+export default function Examples() {
+
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    //console.log(selectedTopic)
+  };
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton isSelected={selectedTopic === 'components'}
+          onSelect={() => handleSelect('components')}>Components</TabButton>
+        <TabButton isSelected={selectedTopic === 'jsx'}
+          onSelect={() => handleSelect('jsx')}>Jsx</TabButton>
+        <TabButton isSelected={selectedTopic === 'props'}
+          onSelect={() => handleSelect('props')}>Props</TabButton>
+        <TabButton isSelected={selectedTopic === 'state'}
+          onSelect={() => handleSelect('state')}>State</TabButton>
+      </menu>
+      {!selectedTopic ? <p>Please selected a topic</p> :
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+              {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+        </div>
+      }
+    </section>
+  )
+}
