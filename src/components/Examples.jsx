747c88d103395a2cc464f9e52f8de5181357f0c5
Elyaquim Nattan <elyaquimnattan@gmail.com>
2024-10-29 09:43:49 -0300

    commit 5

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { EXAMPLES } from '../data-with-examples.js';
 import TabButton from '../components/TabButton';
+import Section from './Section.jsx';
 
 export default function Examples() {
 
@@ -13,17 +14,17 @@ export default function Examples() {
   };
 
   return (
-    <section id="examples">
+    <Section id="examples">
       <h2>Examples</h2>
       <menu>
         <TabButton isSelected={selectedTopic === 'components'}
-          onSelect={() => handleSelect('components')}>Components</TabButton>
+          onClick={() => handleSelect('components')}>Components</TabButton>
         <TabButton isSelected={selectedTopic === 'jsx'}
-          onSelect={() => handleSelect('jsx')}>Jsx</TabButton>
+          onClick={() => handleSelect('jsx')}>Jsx</TabButton>
         <TabButton isSelected={selectedTopic === 'props'}
-          onSelect={() => handleSelect('props')}>Props</TabButton>
+          onClick={() => handleSelect('props')}>Props</TabButton>
         <TabButton isSelected={selectedTopic === 'state'}
-          onSelect={() => handleSelect('state')}>State</TabButton>
+          onClick={() => handleSelect('state')}>State</TabButton>
       </menu>
       {!selectedTopic ? <p>Please selected a topic</p> :
         <div id="tab-content">
@@ -36,6 +37,6 @@ export default function Examples() {
           </pre>
         </div>
       }
-    </section>
+    </Section>
   )
 }
