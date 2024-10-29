edc5f3644652208b14b88224a1980f7ad5741c5c
Elyaquim Nattan <elyaquimnattan@gmail.com>
2024-10-29 10:42:45 -0300

    commit 7

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -16,7 +16,9 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      <Tabs buttons={
+      <Tabs 
+      ButtonsContainer="menu"
+      buttons={
           <>
             <TabButton isSelected={selectedTopic === 'components'}
               onClick={() => handleSelect('components')}>Components</TabButton>
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,8 +1,9 @@
-export default function Tabs({children, buttons}){
+export default function Tabs({children, buttons, ButtonsContainer}){
+  // const ButtonContainer = buttonsContainer;
   return <>
-    <menu>
+    <ButtonsContainer>
     {buttons}
-    </menu>
+    </ButtonsContainer>
     {children}
   </>
 }
